18b999b7e4795072f1d4ef3ce08a0cd95452703f
RZorkin <zorkin81@gmail.com>
2023-06-12 15:32:29 +0300

    Add project files

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -58,7 +58,7 @@ export const ContactForm = () => {
           className={css.input}
           name="name"
           value={name}
-          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)$"
+          pattern="^[a-zA-Zа-яА-Я]+([' -][a-zA-Zа-яА-Я]*)*$"
           title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
           required
           onChange={handleChange}
@@ -72,7 +72,7 @@ export const ContactForm = () => {
           name="number"
           className={css.input}
           value={number}
-          pattern="\\+?\\d{1,4}?[-.\\s]?\\(?\d{1,3}?\\)?[-.\\s]?\\d{1,4}[-.\\s]?\\d{1,4}[-.\\s]?\\d{1,9}"
+          pattern="\\+?\\d{1,4}[-.\\s]?\\(?\d{1,3}\\)?[-.\\s]?\\d{1,4}[-.\\s]?\\d{1,4}[-.\\s]?\\d{1,9}"
           title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
           required
           onChange={handleChange}
